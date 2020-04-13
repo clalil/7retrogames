4ede181a18abe83f7488a005897cac102d139c8e
clalil <cliljander@gmail.com>
2020-04-13 18:36:35 +0200

    Add checkforMatch & flipcard functions

--- memory/app.js
+++ memory/app.js
@@ -52,6 +52,9 @@ document.addEventListener('DOMContentLoaded', () => {
   ]
 
   const gameBoard = document.querySelector('.grid')
+  const chosenCards = []
+  const chosenCardId = []
+  const cardsMatched = []
 
   function createGameBoard() {
     for (let i = 0; i < cardArray.length; i++) {
@@ -60,6 +63,34 @@ document.addEventListener('DOMContentLoaded', () => {
       card.setAttribute('data-id', i)
       //card.addEventListener('click', flipCard())
       gameBoard.appendChild(card)
+    }
+  }
+
+  function checkForMatch() {
+    const cards = document.querySelectorAll('img')
+    const optionOne = chosenCardId[0]
+    const optionTwo = chosenCardId[1]
+    if (chosenCards[0] === chosenCards[1]) {
+      alert('You\'ve found a match!')
+      cards[optionOne].setAttribute('src', 'img/white.png')
+      cards[optionTwo].setAttribute('src', 'img/white.png')
+      cardsMatched.push(chosenCards)
+    } else {
+      cards[optionOne].setAttribute('src', 'img/blank.png')
+      cards[optionTwo].setAttribute('src', 'img/blank.png')
+      alert('Sorry, there\'s no match! Try again!')
+    }
+    chosenCards = []
+    chosenCardId = []
+  }
+
+  function flipCard() {
+    const cardId = this.getAttribyte('data-id')
+    chosenCards.push(cardArray[cardId].name)
+    chosenCardId.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (chosenCards.length === 2) {
+      setTimeout(checkForMatch, 500)
     }
   }
 
