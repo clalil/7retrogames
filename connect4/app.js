a049e90dff2bc11f31109a9b32b73d9b46fe54cc
clalil <cliljander@gmail.com>
2020-05-10 14:34:32 +0200

    Complete game, WIP todo is styling & btn to refresh game

--- connect4/app.js
+++ connect4/app.js
@@ -51,6 +51,20 @@ document.addEventListener('DOMContentLoaded', () => {
         const square2 = squares[winningArrays[y][1]]
         const square3 = squares[winningArrays[y][2]]
         const square4 = squares[winningArrays[y][3]]
+
+        if (square1.classList.contains('player-one') &&
+          square2.classList.contains('player-one') &&
+          square3.classList.contains('player-one') &&
+          square4.classList.contains('player-one')) {
+            result.innerHTML = 'Player One Wins!'
+        } else if (square1.classList.contains('player-two') &&
+        square2.classList.contains('player-two') &&
+        square3.classList.contains('player-two') &&
+        square4.classList.contains('player-two')) {
+          result.innerHTML = 'Player Two Wins!'
       }
+    }
   }
+
+  squares.forEach(square => square.addEventListener('click', checkBoard))
 })
