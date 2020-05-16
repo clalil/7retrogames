28034a62806a169da18a1197109469345c6f159a
clalil <cliljander@gmail.com>
2020-05-16 20:26:39 +0200

    Add style for grid, apple, snake & initial variables

--- snake/app.js
+++ snake/app.js
@@ -7,5 +7,19 @@ function createDivs() {
   }
 }
 
+const square = document.querySelectorAll('.grid div');
+const scoreDisplay = document.querySelector('span');
+const startBtn = document.querySelector('.start');
+
+const width = 10;
+let currentIndex = 0; //first div in our grid
+let appleIndex = 0 //first div in our grid
+let currentSnake = [2, 1, 0] //div in our grid being 2 (Head), 0 being the end (tail), body is all 1s
+let direction = 1;
+let score = 0;
+let speed = 0.9;
+let intervalTime = 0;
+let interval = 0;
+
 createDivs();
 });
