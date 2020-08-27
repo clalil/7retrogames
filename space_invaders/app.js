08b882643dd237ef9b2be31e9742c82909272ced
clalil <cliljander@gmail.com>
2020-08-27 17:38:37 +0200

    Add shoot function

--- space_invaders/app.js
+++ space_invaders/app.js
@@ -51,7 +51,6 @@ document.addEventListener('DOMContentLoaded', () => {
         alienInvaders[i] += direction
       }
       for (let i = 0; i <= alienInvaders.length - 1; i++) {
-      //ADD IF LATER
         if (!alienInvadersTakenDown.includes(i)){
           squares[alienInvaders[i]].classList.add('invader')
         }
@@ -69,7 +68,57 @@ document.addEventListener('DOMContentLoaded', () => {
         clearInterval(invaderId)
       }
     }
+
+    //declare a win
+    if(alienInvadersTakenDown.length === alienInvaders.length) {
+      resultDisplay.textContent = 'You Win'
+      clearInterval(invaderId)
+    }
   }
 
   invaderId = setInterval(moveInvaders, 500)
+
+  function shoot(e) {
+    let laserId
+    let currentLaserIndex = currentShooterIndex
+
+    function moveLaser() {
+      squares[currentLaserIndex].classList.remove('laser')
+      currentLaserIndex -= width
+      squares[currentLaserIndex].classList.add('laser')
+
+      if(squares[currentLaserIndex].classList.contains('invader')) {
+        squares[currentLaserIndex].classList.remove('laser')
+        squares[currentLaserIndex].classList.remove('invader')
+        squares[currentLaserIndex].classList.add('boom')
+
+        setTimeout(() => squares[currentLaserIndex].classList.remove('boom'), 250)
+        clearInterval(laserId)
+
+        const alienTakenDown = alienInvaders.indexOf(currentLaserIndex)
+        alienInvadersTakenDown.push(alienTakenDown)
+        result++
+        resultDisplay.textContent = result
+      }
+
+      if(currentLaserIndex < width) {
+        clearInterval(laserId)
+        setTimeout(() => squares[currentLaserIndex].classList.remove('laser'), 100)
+      }
+    }
+
+    keyup = spacebar
+    document.addEventListener('keyup', e => {
+      if(e.keyCode === 32) {
+        laserId = setInterval(moveLaser, 100)
+      }
+    })
+    // switch(e.keyCode) {
+    //   case 32:
+    //     laserId = setInterval(moveLaser, 100)
+    //     break
+    // }
+
+    document.addEventListener('keyup', shoot)
+  }
 })
