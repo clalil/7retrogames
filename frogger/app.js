ae01186d58f382e1cc25687cc3163b3adeacdb45
clalil <cliljander@gmail.com>
2020-08-29 15:13:26 +0200

    Set initial values

--- frogger/app.js
+++ frogger/app.js
@@ -1,9 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const container = document.querySelector('.grid')[0];
+  const container = document.getElementsByClassName('grid')[0];
   [...Array(81)].forEach((_, div) => {
     div = document.createElement('div');
     container.appendChild(div);
   })
 
+  container.childNodes[4].classList.add('ending-block')
+  container.childNodes[76].classList.add('starting-block')
 
+  const squares = document.querySelectorAll('.grid div')
+  const timeLeft = document.querySelector('#time-left')
+  const result = document.querySelector('#result')
+  const startBtn = document.querySelector('#button')
+  const width = 9
+  let currentIndex = 76
 })
