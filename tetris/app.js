b0bc74e194a3e4d73dceae80408dfba4cd0d8b8f
clalil <cliljander@gmail.com>
2020-09-04 19:02:06 +0200

    Add doc boilerplate, grid & styles & Tetronimoes

---
+++ tetris/app.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementsByClassName('grid')[0];
+  [...Array(200)].forEach((_, div) => {
+    div = document.createElement('div')
+    container.appendChild(div)
+  })
+
+  const grid = document.querySelector('.grid')
+  const width = 10
+  const height = 20
+
+  //The terominos
+
+  //The Tetrominoes
+  const lTetromino = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2]
+  ]
+
+  const zTetromino = [
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1]
+  ]
+
+  const tTetromino = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1]
+  ]
+
+  const oTetromino = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1]
+  ]
+
+  const iTetromino = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3]
+  ]
+})
