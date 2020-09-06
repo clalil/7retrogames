9fe1af29eb48fe7efdfbbb441dda516a3b77e14e
clalil <cliljander@gmail.com>
2020-09-06 14:36:01 +0200

    Add draw/undraw + movement func. + block2 class

--- tetris/app.js
+++ tetris/app.js
@@ -6,8 +6,10 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 
   const grid = document.querySelector('.grid')
+  let squares = Array.from(grid.querySelectorAll('div'))
   const width = 10
   const height = 20
+  let currentPosition = 4
 
   //The terominos
 
@@ -52,4 +54,58 @@ document.addEventListener('DOMContentLoaded', () => {
   let random = Math.floor(Math.random()*theTetrominoes.length)
   let currentRotation = 0
   let current = theTetrominoes[random][currentRotation]
+
+  //draw the shape
+  function draw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.add('block')
+    })
+  }
+
+  //undraw shape
+  function unDraw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.remove('block')
+    })
+  }
+
+  function moveDown() {
+    undraw()
+    currentPosition = currentPosition += width
+    draw()
+    //freeze()
+  }
+
+  //move left and prevent collision with shapes moving left
+  function moveRight() {
+    undraw()
+    const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1)
+    if(!isAtRightEdge) currentPosition += 1
+    if(current.some(index => squares[currentPosition + index].classList.contains('block'))) {
+      currentPosition -= 1
+    }
+    draw()
+  }
+
+  function moveLeft() {
+    undraw()
+    const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+    if(!isAtLeftEdge) currentPosition -= 1
+    if(current.some(index => squares[currentPosition + index].classList.contains('block2'))) {
+      currentPosition += 1
+    }
+    draw()
+  }
+
+  //rotate Tetromino
+  function rotate() {
+    undraw()
+    currentRotation++
+    if(currentRotation === current.length) {
+      currentRotation = 0
+    }
+    current = theTetrominoes[random][currentRotation]
+    draw()
+  }
+
 })
