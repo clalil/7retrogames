7df5d3d44f0d32b49e72a82c6a0205d2a2c19186
clalil <cliljander@gmail.com>
2020-09-12 14:44:11 +0200

    Add freeze function

--- tetris/app.js
+++ tetris/app.js
@@ -156,5 +156,19 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 
-  displayShape()
+  function freeze() {
+    if(current.some(index => squares[currentPosition + index + width].classList.contains('block3'))
+    || squares[currentPosition + index + width].classList.contains('block2')) {
+      current.forEach(index => squares[index + currentPosition].classList.add('block2'))
+
+      random = nextRandom
+      nextRandom = Math.floor(Math.random() * theTetrominoes.length)
+      current = theTetrominoes[random][currentRotation]
+      currentPosition = 4
+      draw()
+      undraw()
+      displayShape()
+    }
+  }
+
 })
