63c27afdcd9549ae5b0b56876595bf8e27696239
clalil <cliljander@gmail.com>
2020-09-27 17:41:13 +0200

    Adjust basic styling, re-write code for reset btn

--- whack-a-mole/app.js
+++ whack-a-mole/app.js
@@ -2,28 +2,31 @@ const container = document.getElementsByClassName('grid')[0];
 let int = 1;
 
 [...Array(9)].forEach((_, div) => {
-  console.log(typeof div)
   div = document.createElement('div')
   div.classList.add('square');
   div.setAttribute('id', int++)
   container.appendChild(div)
 })
 
-const square = document.querySelectorAll('.square');
-const mole = document.querySelector('.mole');
-const timeLeft = document.querySelector('#time-left');
-let score = document.querySelector('#score');
-
-let result = 0;
-let currentTime = timeLeft.textContent;
+const hideTitle = document.querySelectorAll('h4')
+const gameWon = document.querySelector('#result')
+const btn = document.querySelector('button')
+const square = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const timeLeft = document.querySelector('#time-left')
+let score = document.querySelector('#score')
+let result = 0
+let currentTime = timeLeft.textContent
+let timeId
+let moveId
 
 function randomSquare() {
   square.forEach(className => {
-    className.classList.remove('mole');
+    className.classList.remove('mole')
   })
 
-  let randomMolePosition = square[Math.floor(Math.random() * 9)];
-  randomMolePosition.classList.add('mole');
+  let randomMolePosition = square[Math.floor(Math.random() * 9)]
+  randomMolePosition.classList.add('mole')
 
   hitMolePosition = randomMolePosition.id
 }
@@ -38,19 +41,34 @@ square.forEach(id => {
 })
 
 function moveMole() {
-  let timerId = null;
-  timerId = setInterval(randomSquare, 1000)
+  moveId = setInterval(randomSquare, 1000)
+  timeId = setInterval(countDown, 1000)
 }
-
-moveMole();
 
 function countDown() {
   currentTime--;
   timeLeft.textContent = currentTime
   if (currentTime === 0) {
-    clearInterval(timerId);
-    alert("Time's up! Your final score is " + result)
+    clearInterval(timeId);
+    clearInterval(moveId);
+    square.forEach(className => {
+      className.classList.remove('mole')
+    })
+    hideTitle.forEach(el => el.classList.add('hidden'))
+    gameWon.textContent = "Time's up! Your final score is " + result
   }
 }
 
-let timerId = setInterval(countDown, 1000);
+btn.addEventListener('click', () => {
+  clearInterval(timeId);
+  clearInterval(moveId);
+
+  timeLeft.textContent = 30
+  currentTime = 30
+  score.textContent = 0
+  result = 0
+  gameWon.textContent = ''
+  console.log(hideTitle)
+  hideTitle.forEach(el => el.classList.remove('hidden'))
+  moveMole();
+})
